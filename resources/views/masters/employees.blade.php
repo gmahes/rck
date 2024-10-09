43f27efb1e7525ad57b312f91c599ddf24ed7652
gmahes <gregi.mahes@gmail.com>
2024-10-09 16:08:20 +0700

    modal add employee

--- resources/views/masters/employees.blade.php
+++ resources/views/masters/employees.blade.php
@@ -36,8 +36,78 @@
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                         aria-label="Close"></button>
                                                 </div>
-                                                <div class="modal-body">
-                                                    ...
+                                                <div class="modal-body text-start text-dark">
+                                                    <form action="">
+                                                        <div class="mb-3">
+                                                            <div class="row">
+                                                                <div class="col-4">
+                                                                    <label for="username"
+                                                                        class="form-label">Username</label>
+                                                                </div>
+                                                                <div class="col-8">
+                                                                    <input type="text"
+                                                                        class="form-control form-control-sm"
+                                                                        id="username" placeholder="Masukkan Username"
+                                                                        name="username">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <div class="row">
+                                                                <div class="col-4">
+                                                                    <label for="nik" class="form-label">NIK</label>
+                                                                </div>
+                                                                <div class="col-8">
+                                                                    <input type="text"
+                                                                        class="form-control form-control-sm" id="nik"
+                                                                        placeholder="Masukkan NIK" name="nik">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <div class="row">
+                                                                <div class="col-4">
+                                                                    <label for="position"
+                                                                        class="form-label">Jabatan</label>
+                                                                </div>
+                                                                <div class="col-8">
+                                                                    <input type="text"
+                                                                        class="form-control form-control-sm"
+                                                                        id="position" placeholder="Masukkan Jabatan"
+                                                                        name="position">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <div class="row">
+                                                                <div class="col-4">
+                                                                    <label for="role" class="form-label">Role</label>
+                                                                </div>
+                                                                <div class="col-8">
+                                                                    <select class="form-select form-select-sm"
+                                                                        aria-label="role">
+                                                                        <option selected>Pilih Role User</option>
+                                                                        <option value="1">One</option>
+                                                                        <option value="2">Two</option>
+                                                                        <option value="3">Three</option>
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="mt-2">
+                                                            <div class="d-flex flex-row-reverse">
+                                                                <div class="text-end">
+                                                                    <button type="submit"
+                                                                        class="btn btn-sm btn-primary">Simpan</button>
+                                                                </div>
+                                                                <div class="text-end me-2">
+                                                                    <button type="button"
+                                                                        class="btn btn-secondary btn-sm"
+                                                                        data-bs-dismiss="modal">Tutup</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </form>
                                                 </div>
                                             </div>
                                         </div>
